8db63ffd033854c6f93529ed715efe77a9942000
Eklavya Mishra <eklavyamishra@pink-mac.local>
2023-07-20 12:04:03 -0700

    first

---
+++ src/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Sidebar.css"
+
+
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+        Twitter Icon Here
+
+    </div>
+    // sidebar 
+  )
+}
+
+export default Sidebar
