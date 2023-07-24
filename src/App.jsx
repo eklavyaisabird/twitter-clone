0a2554bdd668107fc0ab5f5aca390d51e098a40d
jhoang712 <jessicahoang712@gmail.com>
2023-07-24 16:07:34 -0400

    fixed popup

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,27 @@ import "./App.css";
 import Feed from "./components/Feed";
 import Sidebar from "./components/Sidebar";
 import Widgets from "./components/Widgets";
+import Popup from "./components/Popup"
+import Create from './Create';
+import { useEffect, useState} from 'react';
 
 function App() {
+  const [timedPopup, setTimedPopup] = useState(false);
+  
+  useEffect(() => {
+    setTimeout(()=>{
+      setTimedPopup(true);
+    }, 500);
+  }, []);
+
   return (
     <div className="app">
       <Sidebar />
       <Feed />
       <Widgets />
+      <Popup trigger={timedPopup} setTrigger={setTimedPopup}>
+          <Create />
+      </Popup>
     </div>
   );
 }
