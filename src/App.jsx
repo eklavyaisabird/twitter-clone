164039c0b54f4c396c356ac9506fd1eeda4d05a3
Eklavya Mishra <eklavyamishra@pink-mac.local>
2023-07-24 17:01:04 -0700

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Feed from "./components/Feed";
 import Sidebar from "./components/Sidebar";
 import Widgets from "./components/Widgets";
 
 function App() {
+  const [preferences, setPreferences] = useState(["sports"]);
+
   return (
     <div className="app">
       <Sidebar />
